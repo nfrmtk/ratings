dcfbea6b946f2ae0a92db752bdacb00e09468b73
lykov-ivan-02 <lykov-ivan-02@yandex.ru>
2023-01-03 00:30:02 +0300

    bugfix + test

--- src/v1/post-a-review/view.cpp
+++ src/v1/post-a-review/view.cpp
@@ -32,14 +32,15 @@ class PostReview : public userver::server::handlers::HttpHandlerBase {
     auto text = body["text"].As<std::string>();
     auto game = body["game"].As<std::string>();
     auto username = body["username"].As<std::string>();
-    auto timing = pg_cluster_
+    auto result = pg_cluster_
                       ->Execute(pg::ClusterHostType::kMaster,
                                 "INSERT INTO ratings_schema.reviews(username, "
                                 "game, rating, review) VALUES($1, $2, $3, $4) "
+                                "ON CONFLICT DO NOTHING "
                                 "RETURNING reviews.created_at",
-                                username, game, rating, text)
-                      .AsSingleRow<pg::TimePointTz>();
-
+                                username, game, rating, text);
+    pg::TimePointTz timing;
+    if (!result.IsEmpty()) timing = result.AsSingleRow<pg::TimePointTz>(pg::kFieldTag);
     return userver::utils::datetime::LocalTimezoneTimestring(
         timing.GetUnderlying());
   }
