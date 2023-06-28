7a80dd83576e6f934558febe34f9969ce4742ee1
lykov-ivan-02 <lykov-ivan-02@yandex.ru>
2023-06-28 23:07:51 +0300

    fix components

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,22 @@
 #include <userver/server/handlers/tests_control.hpp>
 #include <userver/testsuite/testsuite_support.hpp>
 #include <userver/utils/daemon_run.hpp>
-
+#include <userver/clients/dns/component.hpp>
+#include <userver/storages/postgres/component.hpp>
+#include "./v1/delete-review/view.hpp"
 #include "./v1/get-reviews/view.hpp"
 #include "./v1/login/view.hpp"
 #include "./v1/post-a-review/view.hpp"
 #include "./v1/register/view.hpp"
 #include "./v1/update-review/view.hpp"
-#include "./v1/delete-review/view.hpp"
 int main(int argc, char* argv[]) {
   auto component_list = userver::components::MinimalServerComponentList()
                             .Append<userver::server::handlers::Ping>()
                             .Append<userver::components::TestsuiteSupport>()
                             .Append<userver::components::HttpClient>()
-                            .Append<userver::server::handlers::TestsControl>();
+                            .Append<userver::server::handlers::TestsControl>()
+                            .Append<userver::clients::dns::Component>()
+                            .Append<userver::components::Postgres>("postgres-db-1");
 
   ratings_service::AppendPostReview(component_list);
   ratings_service::AppendGetReviews(component_list);
