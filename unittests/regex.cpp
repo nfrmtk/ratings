a62e1a340ad98058521d96a5aef082737a927c37
lykov-ivan-02 <lykov-ivan-02@yandex.ru>
2023-07-01 21:55:48 +0300

    fix?

--- unittests/regex.cpp
+++ unittests/regex.cpp
@@ -15,25 +15,3 @@ INSTANTIATE_TEST_SUITE_P(results_true, CorrectEmailTest,
                                            "vasya.pupkin@mail.ru"s,
                                            "123@123456.com"s,
                                            "almaz03@yandex.ru"s));
-
-struct IncorrectEmailTest : ::testing::TestWithParam<std::string> {};
-
-
-UTEST_P(IncorrectEmailTest, pass_false){
-  ASSERT_FALSE(ratings_service::isEmailCorrect(GetParam()));
-}
-
-INSTANTIATE_TEST_SUITE_P(results_false, IncorrectEmailTest,
-                         ::testing::Values("va...sya@pupkin.ru"s,
-                                           "12@3@yahoo.ru"s,
-                                           "#123@123456.com"s,
-                                           "almaz03.@yandex.ru"s,
-                                           "pass@ya/ru"s,
-                                           "pass@ya.c"s,
-                                           "pass@ya"s,
-                                           "pass@ya#.ru"s));
-
-int main(int argc, char* argv[]){
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
