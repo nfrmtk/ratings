fac3b7f43f46c75773fce9352a78dea79bbfc8a5
lykov-ivan-02 <lykov-ivan-02@yandex.ru>
2023-07-01 21:26:12 +0300

    fix?

--- unittests/regex.cpp
+++ unittests/regex.cpp
@@ -6,17 +6,22 @@ using namespace std::literals;
 
 struct CorrectEmailTest : ::testing::TestWithParam<std::string> {};
 
+
+UTEST_P(CorrectEmailTest, pass_true){
+  ASSERT_TRUE(ratings_service::isEmailCorrect(GetParam()));
+}
 INSTANTIATE_TEST_SUITE_P(results_true, CorrectEmailTest,
                          ::testing::Values("vasya@pupkin.ru"s,
                                            "vasya.pupkin@mail.ru"s,
                                            "123@123456.com"s,
                                            "almaz03@yandex.ru"s));
 
-UTEST_P(CorrectEmailTest, pass_true){
-  ASSERT_TRUE(ratings_service::isEmailCorrect(GetParam()));
-}
-
 struct IncorrectEmailTest : ::testing::TestWithParam<std::string> {};
+
+
+UTEST_P(IncorrectEmailTest, pass_false){
+  ASSERT_FALSE(ratings_service::isEmailCorrect(GetParam()));
+}
 
 INSTANTIATE_TEST_SUITE_P(results_false, IncorrectEmailTest,
                          ::testing::Values("va...sya@pupkin.ru"s,
@@ -27,11 +32,6 @@ INSTANTIATE_TEST_SUITE_P(results_false, IncorrectEmailTest,
                                            "pass@ya.c"s,
                                            "pass@ya"s,
                                            "pass@ya#.ru"s));
-
-UTEST_P(IncorrectEmailTest, pass_false){
-  ASSERT_FALSE(ratings_service::isEmailCorrect(GetParam()));
-}
-
 
 int main(int argc, char* argv[]){
   testing::InitGoogleTest(&argc, argv);
