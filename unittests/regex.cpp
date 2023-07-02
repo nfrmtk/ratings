e4ffaefd0d044b70285212002fa8c96c38815df5
lykov-ivan-02 <lykov-ivan-02@yandex.ru>
2023-07-02 18:59:49 +0300

    ci run2

--- unittests/regex.cpp
+++ unittests/regex.cpp
@@ -4,13 +4,13 @@
 using namespace std::literals;
 
 
-struct CorrectEmailTest : ::testing::TestWithParam<std::string> {};
+struct CorrectEmailTest : ::testing::TestWithParam<std::tuple<std::string>> {};
 
-
-UTEST_P(CorrectEmailTest, pass_true){
-  ASSERT_TRUE(ratings_service::isEmailCorrect(GetParam()));
-}
 INSTANTIATE_TEST_SUITE_P(pass_true, CorrectEmailTest,
                          ::testing::Values("vasya@pupkin.ru"s,
                                            "vasya.pupkin@mail.ru"s,
                                            "123@123456.com"s));
+
+UTEST_P(CorrectEmailTest, pass_true){
+  ASSERT_TRUE(ratings_service::isEmailCorrect(std::get<0>(GetParam())));
+}
