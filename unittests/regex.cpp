b309f64f712ab10dba197043e3202fd77631b279
lykov-ivan-02 <lykov-ivan-02@yandex.ru>
2023-07-02 16:37:12 +0300

    ci run

--- unittests/regex.cpp
+++ unittests/regex.cpp
@@ -10,7 +10,7 @@ struct CorrectEmailTest : ::testing::TestWithParam<std::string> {};
 UTEST_P(CorrectEmailTest, pass_true){
   ASSERT_TRUE(ratings_service::isEmailCorrect(GetParam()));
 }
-INSTANTIATE_TEST_SUITE_P(results_true, CorrectEmailTest,
+INSTANTIATE_TEST_SUITE_P(pass_true, CorrectEmailTest,
                          ::testing::Values("vasya@pupkin.ru"s,
                                            "vasya.pupkin@mail.ru"s,
                                            "123@123456.com"s));
