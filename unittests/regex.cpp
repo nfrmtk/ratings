1b56e108ba95f254873c0bca5138bdfe443bc8b3
lykov-ivan-02 <lykov-ivan-02@yandex.ru>
2023-07-02 19:57:41 +0300

    finale) might not work not sure

--- unittests/regex.cpp
+++ unittests/regex.cpp
@@ -4,13 +4,25 @@
 using namespace std::literals;
 
 
-struct CorrectEmailTest : ::testing::TestWithParam<std::string> {};
+struct EmailTest : ::testing::TestWithParam<std::string> {};
 
-INSTANTIATE_UTEST_SUITE_P(pass_true, CorrectEmailTest,
+INSTANTIATE_TEST_SUITE_P(good_emails, EmailTest,
                          ::testing::Values("vasya@pupkin.ru"s,
                                            "vasya.pupkin@mail.ru"s,
                                            "123@123456.com"s));
-
-UTEST_P(CorrectEmailTest, pass_true){
+UTEST_P(EmailTest, good){
   ASSERT_TRUE(ratings_service::isEmailCorrect(GetParam()));
 }
+INSTANTIATE_TEST_SUITE_P(bad_emails, EmailTest,
+                         ::testing::Values("va...sya@pupkin.ru"s,
+                                           "12@3@yahoo.ru"s,
+                                           "#123@123456.com"s,
+                                           "almaz03.@yandex.ru"s,
+                                           "pass@ya/ru"s,
+                                           "pass@ya.c"s,
+                                           "pass@ya"s,
+                                           "pass@ya#.ru"s));
+  UTEST_P(EmailTest, bad){
+  ASSERT_FALSE(ratings_service::isEmailCorrect(GetParam()));
+}
+
