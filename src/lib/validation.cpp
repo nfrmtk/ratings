70d30378f80fe1671b0ed444980b332e4ee432b9
lykov-ivan-02 <lykov-ivan-02@yandex.ru>
2023-07-02 22:15:30 +0300

    fix

--- src/lib/validation.cpp
+++ src/lib/validation.cpp
@@ -4,13 +4,11 @@ namespace ratings_service {
 // https://stackoverflow.com/questions/56907206/using-regex-for-email-input-validation-c
 static const char* email_regex =
     R"([_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4}))";
-template <typename String>
 bool isEmailCorrect(std::string_view email) {
   boost::regex checker{email_regex};
   const std::string email_str = std::string{email};
   return boost::regex_match(email_str, checker);
 }
-template <typename String>
 bool isPasswordStrong(std::string_view password) {
   bool has_big_letters = false, has_numbers = false,
        has_regular_letters = false;
