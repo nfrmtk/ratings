69995bec8be1cc7cad76a04edb2a3f80a0be36c4
lykov-ivan-02 <lykov-ivan-02@yandex.ru>
2023-07-02 21:02:01 +0300

    finale

--- src/lib/validation.cpp
+++ src/lib/validation.cpp
@@ -1,10 +1,9 @@
 #include "validation.hpp"
 #include <regex>
 namespace ratings_service {
-// https://stackabuse.com/validate-email-addresses-with-regular-expressions-in-javascript/
+// https://stackoverflow.com/questions/56907206/using-regex-for-email-input-validation-c
 static const char* email_regex =
-    R"((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])
-)";
+    R"([_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4}))";
 bool isEmailCorrect(const std::string& email) {
   std::regex checker{email_regex};
   return std::regex_match(email, checker);
